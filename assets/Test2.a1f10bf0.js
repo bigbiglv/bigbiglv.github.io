0119028a20ff2a0d81a0115d794aa19b4176ef4f
bigbigking <2845758942@qq.com>
2022-05-19 17:15:57 +0800

    1

--- assets/Test2.a1f10bf0.js
+++
@@ -1,1 +0,0 @@
-import{j as e,k as t,l as a,r as o,q as r,o as s}from"./index.71e0460d.js";const c=r(" test-2 "),_={setup(n){return(p,l)=>(s(),e("div",null,[c,t(a(o),{type:"text",placeholder:"Input"})]))}};export{_ as default};
---
+++ assets/Test2.b59bfbf4.js
@@ -0,0 +1,1 @@
+import{j as e,k as t,l as a,r as o,q as r,o as s}from"./index.800a1ccd.js";const c=r(" test-2 "),_={setup(n){return(p,l)=>(s(),e("div",null,[c,t(a(o),{type:"text",placeholder:"Input"})]))}};export{_ as default};
